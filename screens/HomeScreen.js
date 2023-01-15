0fc2b2491153ea12686c412ea6866f7c2189a5d5
SaFaUU <thulk283@gmail.com>
2023-01-15 16:22:32 +0600

    Added Tailwind Configs and React Navigator

---
+++ screens/HomeScreen.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { StatusBar } from 'expo-status-bar'
+
+const HomeScreen = () => {
+    return (
+        <View>
+            <Text className="text-red-500">HomeScreen</Text>
+        </View>
+    )
+}
+
+export default HomeScreen
