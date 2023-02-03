86093d0e49bd347af6d40d24b4df9f6d922cf428
SaFaUU <thulk283@gmail.com>
2023-02-04 01:01:27 +0600

    Added Quantity option

--- components/DishRow.js
+++ components/DishRow.js
@@ -1,27 +1,63 @@
 import { View, Text, TouchableOpacity, Image } from 'react-native'
 import Currency from "react-currency-formatter"
-import React from 'react'
+import React, { useState } from 'react'
 import { urlFor } from '../sanity'
+import { MinusCircleIcon, PlusCircleIcon } from 'react-native-heroicons/solid'
 
 const DishRow = ({ id, name, description, price, image }) => {
+    const [isPressed, setIsPressed] = useState(false)
     return (
-        <TouchableOpacity>
-            <View>
-                <Text className="text-lg mb-1">{name}</Text>
-                <Text className="text-gray-400">{description}</Text>
-                <Text>
-                    <Currency quantity={price} currency="BDT"> </Currency>
-                </Text>
-            </View>
-            <View>
-                <Image
-                    source={{
-                        uri: urlFor(image).url()
-                    }}
-                    className="h-20 w-20 bg-gray-300 p-4"
-                />
-            </View>
-        </TouchableOpacity>
+        <>
+            <TouchableOpacity onPress={() => setIsPressed(!isPressed)} className={`bg-white border p-4 border-gray-200 ${isPressed && "border-b-0"}`}>
+                <View className="flex-row">
+                    <View className="flex-1 pr-2">
+                        <Text className="text-lg mb-1">{name}</Text>
+                        <Text className="text-gray-400 mt-2">{description}</Text>
+                        <Text>
+                            <Currency quantity={price} currency="BDT"> </Currency>
+                        </Text>
+                    </View>
+                    <View>
+                        <Image
+                            style={{
+                                borderWidth: 1,
+                                borderColor: "#F3F3F4"
+                            }}
+                            source={{
+                                uri: urlFor(image).url()
+                            }}
+                            className="h-20 w-20 bg-gray-300 p-4"
+                        />
+                    </View>
+                </View>
+                {isPressed && <View className="bg-white px-4">
+                    <View className="flex-row items-center space-x-2 pb-3 mt-2">
+                        <TouchableOpacity>
+                            {/* <MinusCircleIcon
+                                color={items.length > 0 ? "#00CCBB" : "gray"}
+                            ></MinusCircleIcon> */}
+                            <MinusCircleIcon
+                                size={40}
+                                color="#00ccbb"
+                            ></MinusCircleIcon>
+                        </TouchableOpacity>
+                        <Text>0</Text>
+                        <TouchableOpacity>
+                            {/* <MinusCircleIcon
+                                color={items.length > 0 ? "#00CCBB" : "gray"}
+                            ></MinusCircleIcon> */}
+                            <PlusCircleIcon
+                                size={40}
+                                color="#00ccbb"
+                            >
+                            </PlusCircleIcon>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+
+                }
+            </TouchableOpacity>
+        </>
     )
 }
 
